6a4bddc01543e621183a0a001515124de804df61
Sam <55955188+SamGrig96@users.noreply.github.com>
2020-12-16 12:59:36 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
   render() {
     return (
 
-      <Router  >
+      <Router  basename='crm/project/'>
             <Switch>
         <Route exact path="/signup" component={SignUp} />
         <Route exact path="/signin" component={SignIn} />
